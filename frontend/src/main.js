7a4fc71cd3059a4985224395358ac82a341d8fc3
whywehere <194068971@qq.com>
2023-06-24 14:32:04 +0800

    add tailwindcss

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import {createPinia} from "pinia";
+import "./assets/tailwind.css"
 const app = createApp(App)
 
 app.use(ElementPlus)
